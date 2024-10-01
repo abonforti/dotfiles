ec13af25e18e31181ed7711b05d947e72e845b1f
Alessio Bonforti <a.bonforti@protonmail.com>
2024-10-01 16:57:56 +0200

    Updated auto password manager and packages

---
+++ .install-password-manager.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    /opt/homebrew/bin/brew install bitwarden-cli
+    ;;
+Linux)
+    wget -O /tmp/bw.zip https://github.com/bitwarden/clients/releases/download/cli-v2024.9.0/bw-linux-2024.9.0.zip
+    unzip /tmp/bw.zip
+    mv /tmp/bw ~/bin/bw
+    chmod +x ~/bin/bw
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
+
