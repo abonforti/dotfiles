bbe39c8373a7e2ad70eb5d084f8c08276c41a389
Alessio Bonforti <a.bonforti@protonmail.com>
2024-10-01 17:30:08 +0200

    Corrected cmd

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -8,10 +8,7 @@ Darwin)
     /opt/homebrew/bin/brew install bitwarden-cli
     ;;
 Linux)
-    curl -L https://github.com/bitwarden/clients/releases/download/cli-v2024.9.0/bw-linux-2024.9.0.zip -o bw.zip && unzip bw.zip -d bw && sudo mv bw/bw /usr/local/bin/bw && sudo chmod +x /usr/local/bin/bw && rm -rf bw.zip bw
-    bw set server https://bw.alessiobonforti.com
-    bw login
-    bw unlock
+    curl -L https://github.com/bitwarden/clients/releases/download/cli-v2024.9.0/bw-linux-2024.9.0.zip -o bw.zip && unzip bw.zip -d bw && sudo mv bw/bw /usr/local/bin/bw && sudo chmod +x /usr/local/bin/bw && rm -rf bw.zip bw && bw config server https://bw.alessiobonforti.com && bw login
     ;;
 *)
     echo "unsupported OS"
